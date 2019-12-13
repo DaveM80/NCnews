f38808f67e47a6d855378e82ed108f4e61c5c89b
DaveM880 <eldeividas@gmail.com>
2019-12-13 11:42:41 +0000

    nc news heroku hosting

--- src/components/styles/StyledSorter.js
+++ src/components/styles/StyledSorter.js
@@ -6,6 +6,10 @@ export const StyledSorter = styled.form`
 display:flex;
 flex-direction: row;
 justify-content: space-around;
+
+@media only screen and (expressions) {
+  CSS-Code;
+}
 div.orderDirRadio{
     display:flex;
     flex-direction: column; 
